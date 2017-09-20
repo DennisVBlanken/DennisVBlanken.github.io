4e7d11ce35120ecc9a2f066b00016fac0cd43e0b
Dennis van Blanken <99040363@mydavinci.nl>
2017-09-20 13:08:47 +0200

    game = gud

--- Snake.js
+++ Snake.js
@@ -9,7 +9,17 @@ for (var row = 1; row <= 15; row++) {
 	}
 }
 
-document.getElementById('slot7-15').innerHTML = "O";
+document.getElementById('slot8-15').innerHTML = "O";
+var begin = document.getElementById('START');
+var started = 0;
+
+function Start(){
+	if (started === 0) {
+	movementLoop();
+	spawnApple();spawnApple();spawnApple();spawnApple();
+	started = 1;
+	begin.style.display = 'none';}
+}
 
 // function for when you lose
 
@@ -29,8 +39,8 @@ function addScore(amount){
 
 // movement
 
-var row = 7;
-var rowNum = 7;
+var row = 8;
+var rowNum = 8;
 var position = 15;
 var slotPos = "slot"+rowNum+"-"+position;
 var passed = 0;
@@ -41,7 +51,8 @@ var nextPosDown = "slot"+(Number(rowNum) + 1)+"-"+position;
 
 function moveRight(){
 	if (position === 30) {Death();}
-	if (document.getElementById(nextPosRight).innerHTML === "A") {tailLength+= 1; addScore(100); spawnApple();}
+	if (document.getElementById(nextPosRight).innerHTML === "A") {
+		tailLength+= 1; addScore(100); spawnApple(); gainSpeed(10);}
 	if (document.getElementById(nextPosRight).innerHTML === "W") {Death();}
 	else if (position < 30) {
 	document.getElementById(slotPos).innerHTML = "W";
@@ -60,7 +71,8 @@ function moveRight(){
 
 function moveLeft(){
 	if (position === 1) {Death();}
-	if (document.getElementById(nextPosLeft).innerHTML === "A") {tailLength+= 1; addScore(100); spawnApple();}
+	if (document.getElementById(nextPosLeft).innerHTML === "A") {
+		tailLength+= 1; addScore(100); spawnApple(); gainSpeed(10);}
 	if (document.getElementById(nextPosLeft).innerHTML === "W") {Death();}
 	else if (position > 1) {
 	document.getElementById(slotPos).innerHTML = "W";
@@ -79,7 +91,8 @@ function moveLeft(){
 
 function moveDown(){
 	if (rowNum === 15) {Death();}
-	if (document.getElementById(nextPosDown).innerHTML === "A") {tailLength+= 1; addScore(100); spawnApple();}
+	if (document.getElementById(nextPosDown).innerHTML === "A") {
+		tailLength+= 1; addScore(100); spawnApple(); gainSpeed(10);}
 	if (document.getElementById(nextPosDown).innerHTML === "W") {Death();}
 	else if (rowNum < 15) {
 	document.getElementById(slotPos).innerHTML = "W";
@@ -99,7 +112,8 @@ function moveDown(){
 
 function moveUp(){
 	if (rowNum === 1) {Death();}
-	if (document.getElementById(nextPosUp).innerHTML === "A") {tailLength+= 1; addScore(100); spawnApple();}
+	if (document.getElementById(nextPosUp).innerHTML === "A") {
+		tailLength+= 1; addScore(100); spawnApple(); gainSpeed(10);}
 	if (document.getElementById(nextPosUp).innerHTML === "W") {Death();}
 	else if (rowNum > 1) {
 	document.getElementById(slotPos).innerHTML = "W";
@@ -117,7 +131,8 @@ function moveUp(){
 	nextPosDown = "slot"+(Number(rowNum) + 1)+"-"+position;
 }
 
-var direction = 3;
+var direction = 2;
+var lastDirection = 2;
 var speed = 450;
 var reds = [];
 var tailLength = 5;
@@ -140,24 +155,17 @@ function spawnApple(){
 
 function movementLoop(){
 	addScore(5);
-	if (direction === 1) {reds[reds.length] = slotPos; moveUp();}
-	if (direction === 2) {reds[reds.length] = slotPos; moveRight();}
-	if (direction === 3) {reds[reds.length] = slotPos; moveDown();}
-	if (direction === 4) {reds[reds.length] = slotPos; moveLeft();}
+	if (direction === 1) {reds[reds.length] = slotPos; moveUp(); lastDirection = 1;}
+	if (direction === 2) {reds[reds.length] = slotPos; moveRight(); lastDirection = 2;}
+	if (direction === 3) {reds[reds.length] = slotPos; moveDown(); lastDirection = 3;}
+	if (direction === 4) {reds[reds.length] = slotPos; moveLeft(); lastDirection = 4;}
 	if (reds.length >= tailLength) {tail();}
 	setTimeout(function() {movementLoop();}, speed);
 }
 
-function gainSpeed(){
-	if (speed >= 200) { speed = speed - 1; setTimeout(function() {gainSpeed();}, 50);}
+function gainSpeed(spd){
+	if (speed >= 151){ speed = speed - spd;}
 }
-
-movementLoop();
-gainSpeed();
-spawnApple();
-spawnApple();
-spawnApple();
-spawnApple();
 
 // all the buttons used with their functions
 
@@ -165,35 +173,35 @@ document.onkeydown = function(e){
     e = e || window.event;
     switch(e.which || e.keyCode){
         case 65: // A
-        	if (direction === 2){return} direction = 4;
+        	if (lastDirection === 2){return} direction = 4;
         break;
 
         case 37: // Left Arrow
-        	if (direction === 2){return} direction = 4;
+        	if (lastDirection === 2){return} direction = 4;
         break;
 
         case 87: // W
-        	if (direction === 3){return} direction = 1;
+        	if (lastDirection === 3){return} direction = 1;
         break;
 
         case 38: // Up Arrow
-        	if (direction === 3){return} direction = 1;
+        	if (lastDirection === 3){return} direction = 1;
         break;
 
         case 68: // D
-        	if (direction === 4){return} direction = 2;
+        	if (lastDirection === 4){return} direction = 2;
         break;
 
         case 39: // Right Arrow
-        	if (direction === 4){return} direction = 2;
+        	if (lastDirection === 4){return} direction = 2;
         break;
 
         case 83: // S
-        	if (direction === 1){return} direction = 3;
+        	if (lastDirection === 1){return} direction = 3;
         break;
 
         case 40: // Down Arrow
-        	if (direction === 1){return} direction = 3;
+        	if (lastDirection === 1){return} direction = 3;
         break;
 
         default: return; // exit this handler for other keys
