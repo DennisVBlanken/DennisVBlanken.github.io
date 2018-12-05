29f637ef7c790c67b2e75f9668afddac5a2aa885
Dennis van Blanken <99040363@mydavinci.nl>
2018-12-05 11:41:40 +0100

    updateeeee

--- js/Snake2.js
+++ js/Snake2.js
@@ -3,35 +3,42 @@ var canvas = document.getElementById('SnakeGame');
 var ctx = canvas.getContext("2d");
 var scoreEl = document.getElementById('score');
 var cellSize = 20;
-var delay = 50;
-var xDelay = canvas.height / 20;
-var yDelay = canvas.width / 20;
-
+var delay = 10;
+var xDelay = (canvas.height / delay);
+var yDelay = (canvas.width / delay);
+var cState = 0;
+var amount = 0;
 
 function DrawGrid(color1 = "#000", color2 = "#23272a") {
 	ctx.fillStyle = color1;
-	var gradient=ctx.createLinearGradient(100,canvas.height,canvas.width,100);
-	gradient.addColorStop("0","violet");
-	gradient.addColorStop("0.14","indigo");
-	gradient.addColorStop("0.28","blue");
-	gradient.addColorStop("0.42","green");
-	gradient.addColorStop("0.57","yellow");
-	gradient.addColorStop("0.71","orange");
-	gradient.addColorStop("0.85","red");
-	gradient.addColorStop("1.0","violet");
+	var gradient=ctx.createLinearGradient(0,canvas.width*2,canvas.width,0);
+for (var i = 0; i <= 15; i++) {
+	switch(cState) {
+	case 0: cState = 1; color = 'purple'; break;
+	case 1: cState = 2; color = 'indigo'; break;
+	case 2: cState = 3; color = 'blue'; break;
+	case 3: cState = 4; color = 'green'; break;
+	case 4: cState = 5; color = 'yellow'; break;
+	case 5: cState = 6; color = 'orange'; break;
+	case 6: cState = 0; color = 'red'; break;
+	default: cState = 0; color = 'red';
+	}
+	amount = i / 15;
+	gradient.addColorStop(amount,color);
+}
 	ctx.strokeStyle = gradient;
 
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.stroke();
 
 	var x = 0.4;
-	var x2 = canvas.width-0.4;
 	var y = 0.4;
-	var y2 = canvas.height-0.4;
-	DrawGridX2(x2);
+	// var x2 = canvas.width-0.4;
+	// var y2 = canvas.height-0.4;
 	DrawGridX(x);
-	DrawGridY2(y2);
 	DrawGridY(y);
+	// DrawGridX2(x2);
+	// DrawGridY2(y2);
 } DrawGrid();
 
 function DrawGridX(x) {
